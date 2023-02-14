f28c83a815c72d5fc71f3377b9b8b804cf76d6f0
Thiago Almeida <tcalmeida85@gmail.com>
2023-02-14 13:07:00 -0300

    feature: add BaseRoutes on server index file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { mySqlConection } from "./database";
 import "dotenv/config";
+import BaseRoutes from "./infra/BaseRoutes"
 
 const app = express();
 
 app.use(express.json());
+app.use(BaseRoutes);
 
 mySqlConection.hasConection();
 
