5df917bb5a5815f15b6600d0c7b8deb6490f9f8f
Thiago Almeida <tcalmeida85@gmail.com>
2023-02-16 12:34:06 -0300

    build: create OrderDetails routes

--- src/infra/BaseRoutes/index.ts
+++ src/infra/BaseRoutes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import userRoutes from "../../modules/User/routes";
 import productRoutes from "../../modules/Product/routes";
 import categoryRoutes from "../../modules/Category/routes";
+import orderRoutes from "../../modules/OrderDetails/routes";
 
 const routes = Router();
 
 routes.use(userRoutes);
 routes.use(productRoutes);
 routes.use(categoryRoutes);
+routes.use(orderRoutes);
 
 export default routes;
