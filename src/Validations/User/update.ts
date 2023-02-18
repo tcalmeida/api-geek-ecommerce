211d17e0d39c12448b55aaf1c2c080d1a0352951
Thiago Almeida <tcalmeida85@gmail.com>
2023-02-18 09:41:57 -0300

    feat: add validation to user routes

---
+++ src/Validations/User/update.ts
@@ -0,0 +1,7 @@
+/* import { param } from "express-validator";
+
+function updateValidation() {
+  return [param("id").isInt().withMessage("id must be a number")];
+}
+
+export default updateValidation(); */
