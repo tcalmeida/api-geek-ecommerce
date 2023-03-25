2eff8c0f3c18bd7acca4bdbcb711c3c4c7217f57
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-03-25 10:28:46 -0300

    fix: remove commented lines (activaded code)

--- src/Middlewares/HandleError.ts
+++ src/Middlewares/HandleError.ts
@@ -1,6 +1,6 @@
 import { ValidationError } from "express-validation";
 import { Request, Response, NextFunction } from "express";
-//const { UnauthorizedError } = require("express-jwt");
+const { UnauthorizedError } = require("express-jwt");
 
 export default (error: any, req: Request, res: Response, next: NextFunction) => {
   try {
@@ -8,9 +8,9 @@ export default (error: any, req: Request, res: Response, next: NextFunction) => 
       return res.status(error.statusCode).json(error);
     }
 
-    /* if (error instanceof UnauthorizedError) {
+     if (error instanceof UnauthorizedError) {
       return res.status(error.status).json(error);
-    } */
+    } 
   } catch (error) {
     return res.status(500).json(error);
   }
