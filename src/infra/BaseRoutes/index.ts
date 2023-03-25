e44c40510464f38c2d130c30697238720883e778
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-03-25 10:31:52 -0300

    update: add login route

--- src/infra/BaseRoutes/index.ts
+++ src/infra/BaseRoutes/index.ts
@@ -3,9 +3,11 @@ import userRoutes from "../../modules/User/routes";
 import productRoutes from "../../modules/Product/routes";
 import categoryRoutes from "../../modules/Category/routes";
 import orderRoutes from "../../modules/Purchase/routes";
+import loginRoute from "../../modules/Login/route"
 
 const routes = Router();
 
+routes.use(loginRoute);
 routes.use(userRoutes);
 routes.use(productRoutes);
 routes.use(categoryRoutes);
