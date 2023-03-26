eec5a6379895c27f806934fdec8e785903c5801e
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-03-26 17:41:29 -0300

    refactor: remove empty lines

--- src/Validations/User/update.ts
+++ src/Validations/User/update.ts
@@ -1,11 +1,3 @@
-/* import { param } from "express-validator";
-
-function updateValidation() {
-  return [param("id").isInt().withMessage("id must be a number")];
-}
-
-export default updateValidation(); */
-
 import{ validate, Joi } from "express-validation";
 
 export default validate({
