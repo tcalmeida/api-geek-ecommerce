e2f90b25a758eec703d56a751e364277ad546c84
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-03-27 10:11:58 -0300

    refactor: product routes path

--- src/modules/Product/routes.ts
+++ src/modules/Product/routes.ts
@@ -1,12 +1,16 @@
-import { Router } from "express";
-import controller from "./controller";
+import { Router } from 'express';
+import controller from './controller';
+import createValidation from '../../validations/Product/create';
+import updateValidation from '../../validations/Product/update';
+import userAuthentication from '../../middlewares/authentication';
+import adminVerification from '../../middlewares/adminVerification';
 
 const routes = Router();
 
-routes.post("/product", controller.create);
-routes.get("/product", controller.findAll);
-routes.get("/product/:id", controller.findOne);
-routes.put("/product/:id", controller.update);
-routes.delete("/product/:id", controller.delete);
+routes.post('/product/admin', userAuthentication,  adminVerification, createValidation, controller.create)
+routes.get('/product/catalog', controller.findAll);
+routes.get('/product/:id', controller.findOne);
+routes.put('/product/admin/:id', userAuthentication, adminVerification, updateValidation, controller.update);
+routes.delete('/product/admin/:id', userAuthentication, adminVerification, controller.delete);
 
 export default routes;
